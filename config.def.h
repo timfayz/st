751ec74a94f243b5a50af2d49a74f47cb2d639a6
Timur Fayzrakhmanov <tim.fayzrakhmanov@gmail.com>
2018-07-20 22:26:56 +0200

    Update config

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "monospace";
 static int borderpx = 2;
 
 /*
